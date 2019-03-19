b390e6317c88bf27db59fea92f00f4ac03dfbc0f
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-03-19 14:19:40 +0000

    query from the form to the DB

--- adminPanel/addformDB.php
+++ adminPanel/addformDB.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 2019-03-19
- * Time: 11:39
- */
+$db = new PDO("mysql:host=192.168.20.20;dbname=PortfolioProjects", 'root', '');
+$db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+
+$title = $_POST['title'];
+$link = $_POST['link'];
+$image = $_POST['image'];
+$sqlProjects = "INSERT INTO `projects` (`title`, `link`, `image`) VALUES (:title, :link, :image);";
+$queryProjects= $db->prepare($sqlAdult);
+$addProject = $queryProjects->execute([':title'=>$title, ':link'=>$link, ':image'=>$image]);
+
+echo 'Project was sucsessfully added';
