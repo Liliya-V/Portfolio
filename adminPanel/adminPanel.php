3f15c96b6f2699d0fe8fe2b0e6785fd1def78a09
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-03-19 13:13:38 +0000

    changing a page title to a relevant one for amin panel and an add object form

--- adminPanel/adminPanel.php
+++ adminPanel/adminPanel.php
@@ -8,7 +8,7 @@ $titles = $query->fetchAll();
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Portfolio Liliya Voevodina</title>
+        <title>Admin panel</title>
         <link href="../css/normalize.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="../css/styleAdminPanel.css" rel="stylesheet" type="text/css">
