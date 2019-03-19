e239487eee626023440086832cb56e6470f6d9e2
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-03-19 10:12:51 +0000

    Created an admin panel, CSSed it

--- adminPanel/adminPanel.php
+++ adminPanel/adminPanel.php
@@ -4,25 +4,22 @@ $db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $sql="SELECT `title` FROM `projects`;";
 $query = $db->query($sql);
 $titles = $query->fetchAll();
-//if(isset($_POST['update'])){
-//    $sqlUpdate = "UPDATE `projects` SET `title`='$_POST[title]', `link`='$_POST[link]', `image`='$_POST[image]';";
-//    $queryUpdate = $db->
-//
-//}
 ?>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>Portfolio Liliya Voevodina</title>
-        <link href="css/normalize.css" rel="stylesheet" type="text/css">
+        <link href="../css/normalize.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="css/styleAdminPanel.css" rel="stylesheet" type="text/css">
+        <link href="../css/styleAdminPanel.css" rel="stylesheet" type="text/css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-<body>
-    <h4>Projects management panel</h4>
-    <?php
-    echo '<table border=1>
+ <body>
+    <h1>Projects management panel</h1>
+      <?php
+        echo '<td><a href="#">Add new projet</a></td>';
+        echo '<br><br>';
+        echo '<table border=5>
         <tr>
             <th>TITLE</th>
             <th colspan="2">ACTION</th>
@@ -30,30 +27,10 @@ $titles = $query->fetchAll();
         foreach ($titles as $title) {
             echo '<tr>';
             echo '<td>' .$title['title']. '</td>';
-            echo '<td><a href="#">Edit</a>';
-            echo '<td><a href="#">Delete</a>';
-        }
+            echo '<td><a href="#">Edit</a></td>';
+            echo '<td><input type="submit" name="delete" value="Delete"></td>';
+           }
         echo '</table>';
-        ?>
-
-
-
-</body>
-
-
-
-<!---->
-<!--    echo 'Projects management panel.';-->
-<!--echo '<br><br><br>';-->
-<!---->
-<!--$sql="SELECT `title` FROM `projects`;";-->
-<!--$query = $db->query($sql);-->
-<!--$titles = $query->fetchAll();-->
-<!-- foreach ($titles as $title) {-->
-<!--     echo '$title['title']'-->
-<!--           <a href="' . $project['link'] . '"><img class="demo" src="'. $project['image'] . '" alt="lamp"></a>-->
-<!--           <h5>'.$project['title'].'</h5>-->
-<!-- };-->
-
-
+      ?>
+ </body>
 </html>
