3e1b65b20476f657830900993891e5f576fe385f
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-03-20 16:10:15 +0000

    created editformdb.php, wrote a code for inserting editeddata into a DB

--- adminPanel/editformdb.php
+++ adminPanel/editformdb.php
@@ -1,4 +1,19 @@
 <?php
+require('functions.php');
+$db = new PDO("mysql:host=192.168.20.20;dbname=PortfolioProjects", 'root', '');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$sqlProjects = "INSERT INTO `projects` (`title`, `link`, `image`) VALUES (:title, :link, :image);";
+$queryProjects= $db->prepare($sqlProjects);
+
+if (validateProject($_POST)) {
+    $addProject = $queryProjects->execute(setVariables($_POST));
+    echo 'Project was successfully edited.';
+    echo '<a href="../portfolio.php">Go to your portfolio. </a>';
+} else {
+    echo 'Please fill all lines';
+    echo '<br><a href="editform.php">Try again</a><br>';
+}
 
 
 
