c67ce958287fb730be678a868ac6a6f2a37a0419
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-03-21 14:46:43 +0000

    removed an sql injection vulnerability, set the if statement for GET id

--- adminPanel/deletebutton.php
+++ adminPanel/deletebutton.php
@@ -4,13 +4,19 @@ $db = new PDO("mysql:host=192.168.20.20;dbname=PortfolioProjects", 'root', '');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
-}
-$sqlDelete = 'DELETE FROM `projects` WHERE `id`= ' . $id . ';';
-$queryDelete= $db->query($sqlDelete );
-if (!$queryDelete) {
-    echo 'An object was not deleted';
+    $sqlDelete = 'DELETE FROM `projects` WHERE `id`=?;';
+    $queryDelete = $db->prepare($sqlDelete);
+    $deleteproject = $queryDelete->execute([$id]);
+    if (!$deleteproject) {
+        echo 'An object was not deleted';
+        echo '<br><a href="adminPanel.php">Try again</a><br>';
+    } else {
+        echo 'Project was successfully deleted.';
+        echo '<br><a href="../portfolio.php">Go to your portfolio.</a><br>';
+        echo '<a href="adminPanel.php">Go back to admin panel .</a>';
+    }
+} else  {
+    echo 'Something went wrong :( ';
     echo '<br><a href="adminPanel.php">Try again</a><br>';
-} else {
-    echo 'Project was successfully deleted.';
-    echo '<a href="../portfolio.php">Go to your portfolio.</a>';
+
 }
